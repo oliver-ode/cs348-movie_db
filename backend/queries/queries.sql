6230a5a67e7522ae9a162578a2cd12fd5669c354
Alishba505 <90855799+Alishba505@users.noreply.github.com>
2024-06-20 01:55:29 -0400

    Submit guess query

--- backend/queries/queries.sql
+++ backend/queries/queries.sql
@@ -96,3 +96,28 @@ LEFT JOIN tagScores ts ON m.mlID = ts.mlID
 LEFT JOIN tagMeaning tm ON ts.tagID = tm.tagID
 WHERE m.mlID = @user_guess_id
 GROUP BY m.mlID, m.mlTitle, m.releaseYear;
+
+--5. Can submit a guess 
+CREATE PROCEDURE InsertUserGuess(
+    IN p_challengeDate VARCHAR(10),
+    IN p_userCookie VARCHAR(36),
+    IN p_mlID INT
+)
+BEGIN
+    DECLARE guessCount INT;
+
+    -- Check the number of guesses the user has made for the given date
+    SELECT COUNT(*) INTO guessCount
+    FROM userPlays
+    WHERE challengeDate = p_challengeDate
+      AND userCookie = p_userCookie;
+
+    -- If the user has made less than 10 guesses, insert the new guess
+    IF guessCount < 10 THEN
+        INSERT INTO userPlays (challengeDate, userCookie, guessNumber, mlID)
+        VALUES (p_challengeDate, p_userCookie, guessCount + 1, p_mlID);
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'User has exceeded the maximum number of guesses for the day';
+    END IF;
+END //
