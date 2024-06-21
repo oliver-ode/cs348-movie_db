77885ef2e96cff28fd1d0df4f3dfb85cbab9921a
Saaniya Saraf <70541474+Saaniya19@users.noreply.github.com>
2024-06-20 21:40:51 -0400

    Update tagQuery.sql

--- backend/queries/tagQuery.sql
+++ backend/queries/tagQuery.sql
@@ -5,9 +5,8 @@ SET @guess_number = 1; -- Adjust guess number as necessary
 
 -- Step 1: Retrieve the tagIDs and tagScores of guessed movie
 WITH guess_tags AS (
-    SELECT t.tagID, ts.score
-    FROM tags t
-    JOIN tagScore ts ON ts.tagID = t.tagID
+    SELECT ts.tagID, ts.score
+    FROM tagScore ts
     JOIN guesses g ON ts.mlID = g.mlID
     WHERE g.challengeDate = @challenge_date 
         AND g.cookie = @cookie
@@ -17,16 +16,21 @@ WITH guess_tags AS (
 
 -- Step 2: Retrieve the tagIDs and tagScores of movie of the day
 WITH motd_tags AS (
-    SELECT t.tagID, ts.score
-    FROM tags t
-    JOIN tagScore ts ON ts.tagID = t.tagID
+    SELECT ts.tagID, ts.score
+    FROM tagScore ts
     JOIN dailyMovies dm ON ts.mlID = dm.mlID
     WHERE dm.challengeDate = @challenge_date AND ts.score > 0.7
 )
 
--- Step 2: Get the top three tags for the movie of the day
-SELECT t.tagID
-FROM guess_tags gt
-JOIN motd_tags mt ON gt.tagID = mt.tagID
-ORDER BY ts.score DESC
+WITH join_tags AS (
+    SELECT gt.tagID, mt.score
+    FROM guess_tags gt
+    JOIN motd_tags mt ON gt.tagID = mt.tagID
+)
+
+-- Step 3: Get the top three tags for the movie of the day
+SELECT t.tagTitle
+FROM tags t
+JOIN join_tags jt ON t.tagID = jt.tagID
+ORDER BY jt.score DESC
 LIMIT 3 \G
