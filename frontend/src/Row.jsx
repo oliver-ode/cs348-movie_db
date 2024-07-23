32edb20294820b4e4ed82c17bf0f2493fba12ae8
Alishba505 <90855799+Alishba505@users.noreply.github.com>
2024-07-23 18:30:37 -0400

    implemented scroll feature

--- frontend/src/Row.jsx
+++ frontend/src/Row.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-
 export default function Row({isCorrect, guess, title, year, yearProximity, casts, genres, tags}) {
-
     return (
         <div className={isCorrect?'row correct':'row'}>
             <div>{guess}</div>
             <div>{title}</div>
-            <div className={isCorrect ? '' : yearProximity=='correct' ? 'correctElement' : ''}>{`${yearProximity=='high'||yearProximity=='low' ? (yearProximity=='high' ? '↓':'↑') : ''} ${year}`}</div>
-            <div>
+            <div className={isCorrect ? '' : yearProximity=='correct' ? 'correctElement' : ''}>
+                {`${yearProximity=='high'||yearProximity=='low' ? (yearProximity=='high' ? '↓':'↑') : ''} ${year}`}
+            </div>
+            <div className="scrollable-container">
                 {casts.map(c => <p className={isCorrect ? '' : c.proximity=='same' ? 'correctElement': c.proximity=='adjacent' ? 'closeElement' : ''}>{c.actorName}</p>)}
             </div>
             <div>
