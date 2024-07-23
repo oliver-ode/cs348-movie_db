24c76c59dc7c5665b231b18b99fdb12ae999aa99
paulinek-lee <pauline.k.lee@gmail.com>
2024-07-23 16:08:00 -0400

    make text colored

--- backend/queries/genreQuery.sql
+++ backend/queries/genreQuery.sql
@@ -1,38 +1,47 @@
--- Set the variables for the challenge date and guessed movie title
-SET @challenge_date = '2024-07-09'; -- Adjust date as necessary
-SET @guessed_movie_title = 'Toy Story'; -- Replace with actual user input
+\
+-- Set the variables for the challenge date and guessed movie title\
+SET @cookie = 'be36fbac-f364-4b4f-b531-4baba05fc596';\
 
--- Step 1: Retrieve the mlID of today's movie
-WITH movie_of_the_day AS (
-    SELECT i.mlID
-    FROM dailyMovies d
-    JOIN tmdbPopularMovies t ON d.selectID = t.selectID
-    JOIN idLinks i ON t.tmdbID = i.tmdbID
-    WHERE d.challengeDate = @challenge_date
-),
+WITH movie_of_the_day AS (\
+    SELECT i.mlID\
+    FROM dailyMovies d\
+    JOIN tmdbPopularMovies t ON d.selectID = t.selectID\
+    JOIN idLinks i ON t.tmdbID = i.tmdbID\
+    WHERE d.challengeDate = CURDATE()\
+),\
+movie_of_the_day_genres AS (\
+    SELECT g.genre\
+    FROM genres g\
+    where g.mlID = (select mlID from movie_of_the_day)\
+),\
+guessed_movie AS (\
+    SELECT g.mlID\
+    FROM guesses g\
+    WHERE g.challengeDate = CURDATE()\
+      AND g.userCookie = @cookie\
+      AND g.guessNumber = (\
+        SELECT COUNT(*) AS mgn\
+        FROM guesses\
+        WHERE userCookie = @cookie\
+        AND challengeDate = CURDATE()\
+      )\
+),\
+guessed_movie_genres AS (\
+    SELECT g.genre\
+    FROM genres g\
+    JOIN guessed_movie gm ON g.mlID = gm.mlID\
+),\
+overlapping_genres AS (\
+    SELECT DISTINCT g.genre\
+    FROM movie_of_the_day_genres g\
+    JOIN guessed_movie_genres gg ON g.genre = gg.genre\
+)\
+SELECT gg.genre,\
+CASE\
+  WHEN gg.genre IN (SELECT genre FROM overlapping_genres) THEN 'same'\
+  WHEN gg.genre NOT IN (SELECT genre FROM overlapping_genres) THEN 'no'\
+END AS proximity\
+FROM guessed_movie_genres gg;
 
--- Step 2: Get genres for the movie of the day
-movie_of_the_day_genres AS (
-    SELECT g.genre
-    FROM genres g
-    JOIN movie_of_the_day m ON g.mlID = m.mlID
-),
 
--- Step 3: Retrieve the mlID of the guessed movie
-guessed_movie AS (
-    SELECT mlID
-    FROM mlMoviesWithYears
-    WHERE mlTitle = @guessed_movie_title
-),
 
--- Step 4: Get genres for the guessed movie
-guessed_movie_genres AS (
-    SELECT g.genre
-    FROM genres g
-    JOIN guessed_movie gm ON g.mlID = gm.mlID
-)
-
--- Step 5: Find the overlapping genres
-SELECT DISTINCT g.genre
-FROM movie_of_the_day_genres g
-JOIN guessed_movie_genres gg ON g.genre = gg.genre;
