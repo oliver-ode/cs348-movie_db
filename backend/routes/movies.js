df7e9becb54faad3a3f94ff8e805677a186fcad5
Oliver Odendaal <oliverode@gmail.com>
2024-07-22 21:32:18 -0400

    Cleanup

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -60,7 +60,6 @@ Router.get("/getFormat", (req, res) => {
 });
 
 
-
 Router.get("/titleSearch", (req, res) => {
   let params = req.query;
   const sql = "SELECT * FROM mlMoviesWithYears WHERE mlTitle LIKE ? LIMIT ?;";
@@ -118,6 +117,7 @@ Router.get("/giveUp", (req, res) => {
     }
   )
 });
+
 
 Router.post("/makeGuess", (req, res) => {
   let body = req.body;
@@ -313,77 +313,5 @@ Router.post("/makeGuess", (req, res) => {
     }
   );
 });
-
-// Router.post("/", (req, res) => {
-//   let qb = req.body;
-//   const sql =
-//     "SET @ID = ?;SET @Name = ?;SET @Position = ?;SET @Team = ?;SET @OpposingTeam = ?;SET @JodySmith = ?;SET @EricMoody = ?;SET @JohnFerguson = ?;SET @FantasyData = ?; CALL Add_or_Update_QB(@ID, @Name, @Position, @Team, @OpposingTeam, @JodySmith, @EricMoody, @JohnFerguson, @FantasyData);";
-//   mysqlConnection.query(
-//     sql,
-//     [
-//       qb.ID,
-//       qb.Name,
-//       qb.Position,
-//       qb.Team,
-//       qb.OpposingTeam,
-//       qb.JodySmith,
-//       qb.EricMoody,
-//       qb.JohnFerguson,
-//       qb.FantasyData,
-//     ],
-//     (err, results, fields) => {
-//       if (!err) {
-//         results.forEach((element) => {
-//           if (element.constructor == Array) res.send(element);
-//         });
-//       } else {
-//         console.log(err);
-//       }
-//     }
-//   );
-// });
-
-// Router.put("/", (req, res) => {
-//   let qb = req.body;
-//   const sql =
-//     "SET @ID = ?;SET @Name = ?;SET @Position = ?;SET @Team = ?;SET @OpposingTeam = ?;SET @JodySmith = ?;SET @EricMoody = ?;SET @JohnFerguson = ?;SET @FantasyData = ?; CALL Add_or_Update_QB(@ID, @Name, @Position, @Team, @OpposingTeam, @JodySmith, @EricMoody, @JohnFerguson, @FantasyData);";
-//   mysqlConnection.query(
-//     sql,
-//     [
-//       qb.ID,
-//       qb.Name,
-//       qb.Position,
-//       qb.Team,
-//       qb.OpposingTeam,
-//       qb.JodySmith,
-//       qb.EricMoody,
-//       qb.JohnFerguson,
-//       qb.FantasyData,
-//     ],
-//     (err, results, fields) => {
-//       if (!err) {
-//         res.send(
-//           "The data for the selected quarterback has been successfully updated."
-//         );
-//       } else {
-//         console.log(err);
-//       }
-//     }
-//   );
-// });
-
-// Router.delete("/:id", (req, res) => {
-//   mysqlConnection.query(
-//     "DELETE FROM quarterback_rankings WHERE ID= ? ",
-//     [req.params.id],
-//     (err, results, fields) => {
-//       if (!err) {
-//         res.send("The selected quarterback has been successfully deleted.");
-//       } else {
-//         console.log(err);
-//       }
-//     }
-//   );
-// });
 
 module.exports = Router;
