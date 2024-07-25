9696af69bc795b607a10cc004355474121d1cbe9
Saaniya19 <saaniya.saraf@gmail.com>
2024-07-25 16:38:38 -0400

    giveup query updated

--- backend/queries/giveUp.sql
+++ backend/queries/giveUp.sql
@@ -1,22 +1,31 @@
-DELIMITER //
-
-CREATE PROCEDURE giveUpFill(
-    IN challenge_date DATE,
-    IN user_cookie CHAR(36)
+WITH movie_of_the_day AS (
+    SELECT i.mlID
+    FROM dailyMovies d
+    JOIN tmdbPopularMovies t ON d.selectID = t.selectID
+    JOIN idLinks i ON t.tmdbID = i.tmdbID
+    WHERE d.challengeDate = CURDATE()
+),
+daily_movie_year AS (
+    SELECT ml.releaseYear
+    FROM mlMoviesWithYears ml
+    JOIN idLinks idl ON ml.mlID = idl.mlID
+    JOIN tmdbPopularMovies tm ON idl.tmdbID = tm.tmdbID
+    JOIN dailyMovies dm ON tm.selectID = dm.selectID
+    WHERE dm.challengeDate = CURDATE()
 )
-begin 
-    declare guessCount int;
-
-    select COUNT(*) into guessCount
-    from guesses
-    where challengeDate = challenge_date
-      and userCookie = user_cookie;
-
-    while guessCount < 10 do
-        insert ignore into guesses(challengeDate, userCookie, guessNumber, mlID)
-        values (challenge_date, user_cookie, -1 * (guessCount + 1) , 1);
-        set guessCount = guessCount + 1;
-    end while;
-end //
-
-DELIMITER ;
+SELECT
+0 AS isCorrect,
+m.mlTitle AS title,
+m.releaseYear AS year,
+'regular' AS yearProximity,
+GROUP_CONCAT(DISTINCT a.actorName ORDER BY a.actorName SEPARATOR ', ') AS casts,
+GROUP_CONCAT(DISTINCT g.genre ORDER BY g.genre SEPARATOR ', ') AS genres,
+GROUP_CONCAT(DISTINCT t.tagTitle ORDER BY ts.score DESC SEPARATOR ', ') AS tags
+FROM mlMoviesWithYears m
+JOIN movie_of_the_day motd ON m.mlID = motd.mlID
+LEFT JOIN idLinks i ON m.mlID = i.mlID
+LEFT JOIN genres g ON m.mlID = g.mlID
+LEFT JOIN imdbActors a ON i.imdbID = a.imdbID
+LEFT JOIN tagScores ts ON m.mlID = ts.mlID
+LEFT JOIN tags t ON ts.tagID = t.tagID
+GROUP BY m.mlID, m.mlTitle, m.releaseYear;
