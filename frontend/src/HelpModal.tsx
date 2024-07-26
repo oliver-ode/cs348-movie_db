354f08dc4dfaddcabf94712aa82b25fe5f7e267a
veronikatadros <veronika.tadros@gmail.com>
2024-07-26 10:44:07 -0400

    Help menu

--- frontend/src/HelpModal.tsx
+++ frontend/src/HelpModal.tsx
@@ -16,7 +16,7 @@ const HelpModal: React.FC<HelpModalProps> = ({ onClose }) => {
         <p>After each guess, the proximity of each element (year, cast, genre, tags) will be shown.</p>
         <h3>Indicators</h3>
         <p><span className="correctElement">Correct</span>: The guessed element is correct.</p>
-        <p><span className="closeElement">Adjacent</span>: The guessed element is close to the element in today's secret movie. For actors, this means that they aren't in the MOTD, but have acted with an actor in the MOTD. For tags/genres, this means that the tag/genre is slightly relateed to the movie, but only has a relevancy score of .5-.7</p>
+        <p><span className="closeElement">Adjacent</span>: The guessed element is close to the element in today's secret movie. For actors, this means that they aren't in the MOTD, but have acted with an actor in the MOTD. For tags, this means that the tag is slightly relateed to the movie, but only has a relevancy score of .5-.7</p>
       </div>
     </div>
   );
